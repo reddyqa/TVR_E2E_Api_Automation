9b50c77c53a9949945f5b6745b6e0ea534082888
Gurender Kush <gurender.kush@nagarro.com>
2021-03-01 16:02:13 +0530

    master city test cases

--- src/main/java/com/MSIL/API_LISTS/City_Master_API.java
+++ src/main/java/com/MSIL/API_LISTS/City_Master_API.java
@@ -4,32 +4,29 @@ import java.util.Hashtable;
 
 import org.testng.ITestContext;
 
+import com.MSIL.JSON_Creation.City_Master_Json;
+
 import Query.Queries;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class All_Generic_MasterAPI 
+public class City_Master_API
 {
 	
-	@SuppressWarnings("unused")
-	public static void generic_master_API(Hashtable<String, String> data, ITestContext context)
+	@SuppressWarnings({ "unused", "static-access" })
+	public static void city_master_API(Hashtable<String, String> data, ITestContext context)
 	{
+		City_Master_Json city_json = new City_Master_Json();
 		Queries q = new Queries();
 		RequestSpecification request = RestAssured.given();
+		String requestBody = city_json.citymaster_create_json(data, context);
 		request.header("Content-Type", "application/json;charset=UTF-8");
 		request.header("x-app-id", "4");
 		request.header("Authorization", context.getAttribute("Authorization"));
+		request.body(requestBody);
 		//System.out.println(context.getAttribute("Authorization"));
-		Response response = request.queryParam("model", data.get("model"))
-				.queryParam("variant", data.get("variant"))
-				.queryParam("color", data.get("color"))
-				.queryParam("bodyType", data.get("bodyType"))
-				.queryParam("channelType", data.get("channelType"))
-				.queryParam("registrationType", data.get("registrationType"))
-				.queryParam("transmissionType", data.get("transmissionType"))
-				.queryParam("fuelType", data.get("fuelType"))
-				.get(data.get("endpoint"));
+		Response response = request.post(data.get("endpoint"));
 		float f = Float.parseFloat(data.get("expectedErrorCode"));
 		int v = (int) f;
 		System.out.println("Response of all master API: "+response.getBody().asString());
---
+++ src/main/java/com/MSIL/POJO/City_Master_POJO.java
@@ -0,0 +1,45 @@
+package com.MSIL.POJO;
+
+public class City_Master_POJO
+{
+	private String cityId;
+	private String regType;
+	private String isNew;
+	private String currentDate;
+	private String isFuture;
+	
+	public String getCityId() {
+		return cityId;
+	}
+	public void setCityId(String cityId) {
+		this.cityId = cityId;
+	}
+	public String getRegType() {
+		return regType;
+	}
+	public void setRegType(String regType) {
+		this.regType = regType;
+	}
+	public String getIsNew() {
+		return isNew;
+	}
+	public void setIsNew(String isNew) {
+		this.isNew = isNew;
+	}
+	public String getCurrentDate() {
+		return currentDate;
+	}
+	public void setCurrentDate(String currentDate) {
+		this.currentDate = currentDate;
+	}
+	public String getIsFuture() {
+		return isFuture;
+	}
+	public void setIsFuture(String isFuture) {
+		this.isFuture = isFuture;
+	}
+	
+	
+	
+
+}
